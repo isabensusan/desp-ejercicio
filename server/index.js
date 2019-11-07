9f7ee4845b448175de3f7161d8a6f310246a501c
isabensusan <isabensusan@gmail.com>
2019-11-07 19:27:11 -0300

    remove gitignore files

--- server/index.js
+++ server/index.js
@@ -9,12 +9,10 @@ const app = express();
 
 const port = process.env.PORT || 5000;
 
-//connect to the db
 mongoose.connect(process.env.DB, { useNewUrlParser: true })
   .then(() => console.log(`Database connected successfully`))
   .catch(err => console.log(err));
 
-//since mongoose promise is depreciated, we overide it with node's promise
 mongoose.Promise = global.Promise;
 
 app.use((req, res, next) => {
